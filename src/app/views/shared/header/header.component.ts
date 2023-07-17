9b6eccd03c2631164b90e806b513be54deea8b27
wallace <wallace_wolv@hotmail.com>
2023-07-17 15:55:46 -0300

    feat: Criando HeaderComponent

--- src/app/views/shared/header/header.component.ts
+++ src/app/views/shared/header/header.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'fbank-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  standalone: true
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
+  providers: [DatePipe],
+  standalone: true,
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+  public currentDate = '';
+  public notifications = [];
+
+  constructor(private datePipe: DatePipe) {}
+
+  ngOnInit(): void {
+    this.formattedDate();
+  }
+  private formattedDate() {
+    const date = new Date();
+    this.currentDate = this.datePipe.transform(date, 'MMM dd, yyyy') ?? '';
+  }
 }
