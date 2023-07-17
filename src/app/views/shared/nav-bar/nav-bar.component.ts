d627a88aca72a8d8567de6fc8ce28b94bd618a95
wallace <wallace_wolv@hotmail.com>
2023-07-17 12:28:03 -0300

    feat: Criando NavBarComponent

--- src/app/views/shared/nav-bar/nav-bar.component.ts
+++ src/app/views/shared/nav-bar/nav-bar.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fbank-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
-  standalone: true
+  imports: [CommonModule],
+  standalone: true,
 })
 export class NavBarComponent implements OnInit {
+  menus = [
+    { url: './assets/images/Overview.svg', name: 'Overview', active: true },
+    { url: './assets/images/Wallet.svg', name: 'Balances', active: false },
+    {
+      url: './assets/images/Transaction.svg',
+      name: 'Transactions',
+      active: false,
+    },
+    { url: './assets/images/Bill.svg', name: 'Bills', active: false },
+    { url: './assets/images/Expencces.svg', name: 'Expenses', active: false },
+    { url: './assets/images/Goal.svg', name: 'Goals', active: false },
+    { url: './assets/images/Settings.svg', name: 'Settings', active: false },
+  ];
+
   constructor() {}
+
   ngOnInit(): void {}
 }
