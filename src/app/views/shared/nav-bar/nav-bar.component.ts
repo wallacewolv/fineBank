6ef94824df6fae9e13449061e9b6f13c34b790cc
wallace <wallace_wolv@hotmail.com>
2023-07-16 23:34:55 -0300

    feat: Criando componente dashboard

---
+++ src/app/views/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fbank-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+  standalone: true
+})
+export class NavBarComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
