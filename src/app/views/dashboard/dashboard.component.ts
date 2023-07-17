f49dcafe1971a13b7f40006ed736e1e219b28871
wallace <wallace_wolv@hotmail.com>
2023-07-17 18:28:27 -0300

    feat: Incluindo drag and drop no dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'fbank-dashboard',
@@ -8,4 +14,27 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {}
+  progress = ['Carrots', 'Tomatoes', 'Onions'];
+  todo = ['Oranges', 'Bananas', 'Cucumbers'];
+  done = ['Watermelon', 'Apple', 'Peach'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      console.log('mudar no container');
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      console.log('mudar no outro container');
+
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -4,14 +4,21 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedComponentModule } from '../shared/shared-component.module';
 import { DashboardComponent } from './dashboard.component';
-
+import { NgFor } from '@angular/common';
+import {
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    SharedComponentModule
-  ]
+    SharedComponentModule,
+    CdkDropList,
+    NgFor,
+    CdkDrag,
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
