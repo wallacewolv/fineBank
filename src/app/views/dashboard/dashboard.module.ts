6f5bcde71a47986bbb4e89daee7a3d5184785b3d
wallace <wallace_wolv@hotmail.com>
2023-07-20 13:13:44 -0300

    feat: Conteudo do UpcommingBillsComponent

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -5,17 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { SharedComponentModule } from '../shared/shared-component.module';
+import { BalancesComponent } from './components/balances/balances.component';
+import { OverviewComponent } from './components/overview/overview.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { BalancesComponent } from './components/balances/balances.component';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    OverviewComponent,
-    BalancesComponent,
-  ],
+  declarations: [DashboardComponent, OverviewComponent, BalancesComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
