c87ad8c718432b02838fcfb52a8bc8303cef7ca3
wallace <wallace_wolv@hotmail.com>
2023-07-20 00:35:08 -0300

    feat: Conteudo do TotalBalance e obervando posição na tela

--- src/app/views/dashboard/components/balances/balances.component.ts
+++ src/app/views/dashboard/components/balances/balances.component.ts
@@ -1,15 +1,41 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fbank-balances',
   templateUrl: './balances.component.html',
-  styleUrls: ['./balances.component.scss']
+  styleUrls: ['./balances.component.scss'],
 })
 export class BalancesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  drop(event: CdkDragDrop<string[]> | any) {
+    // console.log('Começou a mover: ', event.event.target.id);
+    // console.log('offsetTop: ', event.event.target.offsetTop);
+    // console.log('offsetLeft: ', event.event.target.offsetLeft);
   }
 
+  dropEnd(event: CdkDragDrop<string[]> | any) {
+    console.log(event);
+
+    // Obter a posição do elemento
+    const posicaoX: number = event.event.x;
+    const posicaoY: number = event.event.y;
+
+    // Exibir a posição
+    console.log('Posição X: ' + posicaoX);
+    console.log('Posição Y: ' + posicaoY);
+
+    const larguraTotal: number = event.source.element.nativeElement.offsetWidth;
+    const alturaTotal: number = event.source.element.nativeElement.offsetHeight;
+
+    // Exibir o tamanho total
+    console.log('Largura Total: ' + larguraTotal + 'px');
+    console.log('Altura Total: ' + alturaTotal + 'px');
+
+    const posicaoFinal = event.source.getFreeDragPosition();
+    console.log('Posição final:', posicaoFinal);
+  }
 }
