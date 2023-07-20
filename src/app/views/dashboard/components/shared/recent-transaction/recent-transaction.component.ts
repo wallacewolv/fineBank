6f9c4e66a6ba5a260d3469f333377578cd2b836a
wallace <wallace_wolv@hotmail.com>
2023-07-20 15:36:50 -0300

    feat: Conteudo do RecentTransaction

--- src/app/views/dashboard/components/shared/recent-transaction/recent-transaction.component.ts
+++ src/app/views/dashboard/components/shared/recent-transaction/recent-transaction.component.ts
@@ -1,16 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { cargaRecentsTransactions } from 'src/assets/carga';
+
+export type CargaRecentsTransactionsType = {
+  urlImage: string;
+  product: string;
+  productDesc: string;
+  price: string;
+  dateTransaction: string;
+};
 
 @Component({
   selector: 'RecentTransaction',
   templateUrl: './recent-transaction.component.html',
   styleUrls: ['./recent-transaction.component.scss'],
+  imports: [CommonModule],
   standalone: true,
 })
 export class RecentTransactionComponent implements OnInit {
-
-  constructor() { }
+  public recentsTransactions!: Array<CargaRecentsTransactionsType>;
+  constructor() {}
 
   ngOnInit() {
+    this.recentsTransactions = cargaRecentsTransactions;
   }
-
 }
